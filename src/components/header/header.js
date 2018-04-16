059a76ba42167530c337986c6a03f617b323528e
Jody Duke <contact@jodyduke.co.uk>
2018-04-16 14:41:45 +0100

    added date changing handlers

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import { numToMonth } from '../utils/num_to_month';
+import ChevronLeft from '../../images/chevron_left';
+import ChevronRight from '../../images/chevron_right';
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick = (data) => {
+        this.props.handleDateChange(data)
+    }
+
     render() {
         return (
-            <div className="header">howdy</div>
+            <div className="header">
+                <div className="month">
+                    <button onClick={() => this.handleClick('monthDown')}><ChevronLeft/></button>{numToMonth(this.props.month)}<button onClick={() => this.handleClick('monthUp')}><ChevronRight/></button>
+                </div>
+
+                <div className="year">
+                    <button onClick={() => this.handleClick('yearDown')}><ChevronLeft /></button>{this.props.year}<button onClick={() => this.handleClick('yearUp')}><ChevronRight /></button>
+                </div>
+            </div>
         )
     }
 }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,34 +7,72 @@ class Main extends Component {
         super(props)
         this.state = {
             currentMonth : null,
-            currentYear : null
+            currentYear : null,
+            firstDayOfMonth : null
         }
+
+        this.handleDateChange = this.handleDateChange.bind(this)
     }
 
-componentDidMount() {
-    const todaysDate = new Date();
-    this.setState({
-        currentMonth: parseInt(todaysDate.getMonth().toString(), 10) + 1,
-        currentYear: todaysDate.getYear().toString()
-    })
+    componentDidMount() {
+        const todaysDate = new Date();
+        this.setState({
+            currentMonth: parseInt(todaysDate.getMonth().toString(), 10),
+            currentYear: todaysDate.getFullYear()
+        })
+    } 
 
-}
+        handleDateChange = (data) => {
+            let newMonth = this.state.currentMonth
+            let newYear = this.state.currentYear
+            if(data === 'monthDown') {
+                if (this.state.currentMonth === 0) {
+                    newMonth = 11
+                    newYear--;
+                }
+                else {
+                    newMonth--;
+                }
+            }           
+            if(data === 'monthUp') {
+                if (this.state.currentMonth === 11) {
+                    newMonth = 0
+                    newYear++
+                }
+                else {
+                    newMonth++;
+                }
+            }
+            if(data === 'yearUp') {
+                newYear ++
+            }
+            if(data === 'yearDown') {
+                newYear --
+            }     
+            this.setState({
+                currentMonth : newMonth,
+                currentYear : newYear
+            })
+        }
+    
+
 
     render() {
-        const calendar = calendarMap(this.state.currentMonth, this.state.currentYear);
+        const calendarArray = calendarMap(this.state.currentMonth, this.state.currentYear);
 
         return (
             <div className="main">
-                <Header month={this.state.currentMonth} year={this.state.currentYear}/>
+                <Header handleDateChange={this.handleDateChange} month={this.state.currentMonth} year={this.state.currentYear}/>
                 <div className="calendar">
-                    <div className="days">Sun</div>
                     <div className="days">Mon</div>                  
                     <div className="days">Tue</div>                  
                     <div className="days">Wed</div>                  
                     <div className="days">Thu</div>                  
                     <div className="days">Fri</div>                  
-                    <div className="days">Sat</div>                  
-                    {calendar}
+                    <div className="days">Sat</div>  
+                    <div className="days">Sun</div>
+                                    
+                    {calendarArray}
                 </div>
             </div>
         )
