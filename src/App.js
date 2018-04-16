0d1c81506d7bf54d29c535cfc428c9be125c0472
Jody Duke <contact@jodyduke.co.uk>
2018-04-16 16:18:01 +0100

    moved resize listener to parent app component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,41 @@ import Menu from './components/menu/menu';
 import Main from './components/main/main';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      width: 'desktop'
+    }
+  }
+
+  updateWidth() {
+    if (window.innerWidth <= 768) {
+      this.setState({
+        width: 'mobile'
+      })
+    }
+    else {
+      this.setState({
+        width: 'desktop'
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.updateWidth();
+    window.addEventListener("resize", this.updateWidth.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth.bind(this))
+  }
+
   render() {
     return (
       <div className="app">
-        <Menu />
-        <Main />
+        <Menu responsive={this.state.width}/>
+        <Main responsive={this.state.width}/>
       </div>
     );
   }
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,40 +3,10 @@ import MenuMobile from './responsive_components/menu_mobile';
 import MenuDesktop from './responsive_components/menu_desktop';
 
 class Menu extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            collapsed: true,
-            width: 'desktop',
-        }
-    }
-
-    updateWidth(){
-        if(window.innerWidth <= 768) {
-            this.setState({
-                width: 'mobile'
-            })
-        }
-        else {
-            this.setState({
-                width: 'desktop'
-            })
-        }
-    }
-
-    componentDidMount() {
-        this.updateWidth();
-        window.addEventListener("resize", this.updateWidth.bind(this))
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWidth.bind(this))
-    }
-
     render() {
         return (
             <div className="menu">
-                {this.state.width === 'mobile' ? <MenuMobile /> : <MenuDesktop />}
+                {this.props.responsive === 'mobile' ? <MenuMobile /> : <MenuDesktop />}
             </div>
         )
     }
