331fd9ccb48c8e269a6f8d48ebd20edaf8d90271
Jody Duke <contact@jodyduke.co.uk>
2018-04-17 14:54:30 +0100

    added infinite scroll to mobile view

--- src/components/main/calendar_node.js
+++ src/components/main/calendar_node.js
@@ -1,10 +1,13 @@
 import React,{ Component } from "react";
+import { numToMonth } from '../utils/num_to_month';
 
 export default class CalendarNode extends Component {
     render() {
         return (
             <div className={this.props.class}>
                 <h4>{this.props.day}</h4>
+                <h3>{numToMonth(this.props.month)}</h3>
+                <h3>{this.props.year}</h3>
             </div>
         )
     }
--- src/components/main/responsive_components/calendar_mobile.js
+++ src/components/main/responsive_components/calendar_mobile.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { calendarMap } from '../../utils/map_calendar';
+import CalendarNode from "../calendar_node";
 
 class CalendarMobile extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            calendarArr : [],
+            mapKey : 10000
+        }
         this.handleScroll = this.handleScroll.bind(this)
     }
 
 
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll)
+        const calendarArray = calendarMap(this.props.data.currentMonth, this.props.data.currentYear);
+        this.setState({
+            calendarArr: calendarArray
+        })
     }
 
     componentWillUnmount() {
@@ -18,15 +27,22 @@ class CalendarMobile extends Component {
 
     handleScroll = (ev) => {
         if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-            console.log("you're at the bottom of the page");
+            this.props.handleDateChange('monthUp')
+            const calendarUpdate = calendarMap(this.props.data.currentMonth, this.props.data.currentYear);
+            let currentCalendar = this.state.calendarArr
+            currentCalendar.push(<CalendarNode key={this.state.mapKey} class="mobile-month-year" month={this.props.data.currentMonth} year={this.props.data.currentYear}/>)
+            currentCalendar.push(calendarUpdate)
+            this.setState({
+                calendarArr : currentCalendar,
+                mapKey: this.state.mapKey + 1
+            })
         }
     };
 
     render() {
-        const calendarArray = calendarMap(this.props.data.currentMonth, this.props.data.currentYear);
         return (
             <div className="calendar">
-                {calendarArray}
+                {this.state.calendarArr}
             </div>
         )
     }
