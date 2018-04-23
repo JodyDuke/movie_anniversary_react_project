f7be7e054e4aab58551f711d77c12dc2cead91b5
Jody Duke <contact@jodyduke.co.uk>
2018-04-23 15:57:16 +0100

    added mapping function for movie titles

--- src/components/main/calendar_node.js
+++ src/components/main/calendar_node.js
@@ -1,16 +1,18 @@
 import React,{ Component } from "react";
 import { numToMonth } from '../utils/num_to_month';
 
-
+const TitleMap = (props) => {
+    const titles = props.data
+    const listTitles = titles.map((e, k) => <p key={k}>{e.title}</p>)
+    return <div>{listTitles}</div>
+}
 
 export default class CalendarNode extends Component {
-
     render() {
-        console.log(this.props.titles)
-
         return (
             <div className={this.props.class}>
                 <h4>{this.props.day}</h4>
+                {this.props.titles ? <TitleMap data={this.props.titles} /> : <div></div>}
                 <h3>{numToMonth(this.props.month)}</h3>
                 <h3>{this.props.year}</h3>
             </div>
