c72f19b32c7912a003530d27b4769e2c72914f68
Jody Duke <contact@jodyduke.co.uk>
2018-04-24 14:02:22 +0100

    added region and release type to query string

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -3,7 +3,7 @@ export const tmdb = {
     discover: 'discover/movie?',
     startString: '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=',
     releaseLessThan: '&primary_release_date.lte=',
-    endString: '&vote_average.gte=5&without_genres=10402%2C99%2C10770&with_original_language=en'
+    endString: '&vote_average.gte=5&without_genres=10402%2C99%2C10770&with_original_language=en&region=GB&with_release_type=3|1'
 
 }
 
