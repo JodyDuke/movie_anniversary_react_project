36d47354c5eab5e56078047571bae26375bcc8fd
Jody Duke <contact@jodyduke.co.uk>
2018-04-24 14:03:28 +0100

    added function to map birthday and day to json data and update render

--- src/components/main/responsive_components/calendar_desktop.js
+++ src/components/main/responsive_components/calendar_desktop.js
@@ -12,35 +12,50 @@ class CalendarDesktop extends Component {
 
     componentDidMount() {
         this.setState({
-            calendarArr: calendarMap(this.props.month, this.props.year)
+            calendarArr: calendarMap(this.props.month, this.props.year),
         })
     }
 
-    // componentWillReceiveProps(nextProps) { 
+    componentWillReceiveProps(nextProps) { 
 
-    //     if(nextProps.data.results !== undefined){
+        //if data is received from api call then block will fire
+       if(nextProps.data[0] !== undefined){
 
-    //         let newData = nextProps.data.results;
+        let newData = nextProps.data
 
-    //         //adds a new data point to api data to match days in map_calendar function
-    //         let newDataMapped = newData.map((data, int) => {
-    //             let day = parseInt(data.release_date.slice(-2), 10)
-    //             data.day = day
-    //             return data
-    //         })
+        //returns all results from api call
+        let mapped = newData.map(e => {
+            return e.results
+        })
+
+        //reduces all results into one array for easier mapping
+        mapped = mapped.concat.apply([], mapped);
+        console.log(mapped)
+
+            //adds a new data point to api data to match days and anniversary year in map_calendar function
+            let newDataMapped = mapped.map((data, int) => {
+                //console.log(data)
+                if(data.popularity >= 9){
+                let releaseYear = parseInt(data.release_date.slice(0, 4), 10)
+                let day = parseInt(data.release_date.slice(-2), 10)
+                data.day = day
+                data.birthday = this.props.year - releaseYear
+                return data
+                }
+                else return ''
+            })
 
             
-    //         this.setState({
-    //             calendarArr : calendarMap(nextProps.month, nextProps.year, newDataMapped)
-    //         })
+            this.setState({
+                calendarArr : calendarMap(nextProps.month, nextProps.year, newDataMapped)
+            })
 
-    //     }
+       }
         
-    // }
+    }
 
-
-
-    render() {      
+    render() {  
+        //let mapped = this.props.data.map(e => console.log(e))
         const daysMap = days.map((item, i) => <div className="days" key={i}>{item}</div>)
         return (
             <div className="calendar">
