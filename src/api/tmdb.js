9e6dd671e0244464cf0befd781beb906748bf153
Jody Duke <contact@jodyduke.co.uk>
2018-04-24 18:24:20 +0100

    fixed release dates, set to UK

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,9 +1,9 @@
 export const tmdb = {
     url: 'https://api.themoviedb.org/3/',
     discover: 'discover/movie?',
-    startString: '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=',
-    releaseLessThan: '&primary_release_date.lte=',
-    endString: '&vote_average.gte=5&without_genres=10402%2C99%2C10770&with_original_language=en&region=GB&with_release_type=3|1'
+    startString: '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.gte=',
+    releaseLessThan: '&release_date.lte=',
+    endString: '&vote_average.gte=5&without_genres=10402%2C99%2C10770&with_original_language=en&region=GB&with_release_type=1|2|3'
 
 }
 
