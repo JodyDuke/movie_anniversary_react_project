25a96e125f5477f3090d549e3358c38919c9fade
Jody Duke <contact@jodyduke.co.uk>
2018-04-25 14:51:09 +0100

    added image handling

--- src/components/main/calendar_node.js
+++ src/components/main/calendar_node.js
@@ -1,5 +1,6 @@
 import React,{ Component } from "react";
 import { numToMonth } from '../utils/num_to_month';
+import { tmdb } from '../../api/tmdb';
 
 const TitleMap = (props) => {
     const titles = props.data
@@ -9,12 +10,20 @@ const TitleMap = (props) => {
 
 export default class CalendarNode extends Component {
     render() {
+         let poster;
+        if(this.props.titles !== undefined && this.props.titles.length > 0){
+            poster = tmdb.images.secure_base_url + tmdb.images.poster_sizes[5] + this.props.titles[0].poster_path
+        }
+
+
         return (
-            <div className={this.props.class}>
-                <h4>{this.props.day}</h4>
-                {this.props.titles ? <TitleMap data={this.props.titles} /> : null}
-                <h3>{numToMonth(this.props.month)}</h3>
-                <h3>{this.props.year}</h3>
+            <div className="background-images" style={{backgroundImage: `url(${poster})`}}>
+                <div className={this.props.class}>
+                    <h4>{this.props.day}</h4>
+                    {this.props.titles ? <TitleMap data={this.props.titles} /> : null}
+                    <h3>{numToMonth(this.props.month)}</h3>
+                    <h3>{this.props.year}</h3>
+                </div>
             </div>
         )
     }
