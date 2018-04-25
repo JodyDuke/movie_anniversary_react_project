77c9a70eb9131956b1a4823d5ccf2d693c7ffea6
Jody Duke <contact@jodyduke.co.uk>
2018-04-25 12:36:32 +0100

    added logic to weigh popularity more for older movies

--- src/components/utils/add_birthday_map.js
+++ src/components/utils/add_birthday_map.js
@@ -7,12 +7,12 @@ export const addBirthday = (input) => {
 
     //adds a new data point to api data to match days and anniversary year in map_calendar function
     let newDataMapped = mapped.map((data, int) => {
-        //console.log(data)
-        if (data.popularity >= 9) {
-            let releaseYear = parseInt(data.release_date.slice(0, 4), 10)
+        let releaseYear = parseInt(data.release_date.slice(0, 4), 10)
+        let birthday = input.year - releaseYear
+        if ((birthday <= 10 && data.popularity >= 10) || (birthday > 10 && data.popularity >= 8)) {
             let day = parseInt(data.release_date.slice(-2), 10)
             data.day = day
-            data.birthday = input.year - releaseYear
+            data.birthday = birthday
             return data
         }
         else return ''
