a4c2c3ccb2243db2465ca99abe1aee8b64752ec0
Jody Duke <contact@jodyduke.co.uk>
2018-04-26 15:36:52 +0100

    moved render of data up to parent component

--- src/components/main/responsive_components/calendar_desktop.js
+++ src/components/main/responsive_components/calendar_desktop.js
@@ -1,7 +1,9 @@
 import React,{ Component } from "react";
+import { Route } from "react-router-dom";
 import {calendarMap} from '../../utils/map_calendar';
 import { days } from '../../utils/days';
-import { addBirthday } from '../../utils/add_birthday_map'
+import { addBirthday } from '../../utils/add_birthday_map';
+
 
 class CalendarDesktop extends Component {
     constructor(props){
@@ -11,28 +13,49 @@ class CalendarDesktop extends Component {
         }
     }
 
-    componentDidMount() {
-        this.setState({
-            calendarArr: calendarMap(this.props.month, this.props.year),
-        })
-    }
 
-    componentWillReceiveProps(nextProps) { 
-        //if data is received from api call then block will fire
-       if(nextProps.data[0] !== undefined){         
-            this.setState({
-                calendarArr: calendarMap(nextProps.month, nextProps.year, addBirthday(nextProps))
-            })
-       }       
-    }
+    // componentDidMount() {
+    //     this.setState({
+    //         calendarArr: calendarMap(this.props.month, this.props.year),
+    //     })
+    // }
+
+    // componentWillReceiveProps(nextProps) { 
+    //     //if data is received from api call then block will fire
+    //    if(nextProps.data[0] !== undefined){         
+    //         this.setState({
+    //             calendarArr: calendarMap(nextProps.month, nextProps.year, addBirthday(nextProps))
+    //         })
+    //    }       
+    // }
+
+    // componentWillReceiveProps(nextProps) {
+    //     if(nextProps.data !== undefined && nextProps.data.length > 0){
+    //         this.setState({
+    //             calendarArr: nextProps.data[0].titles
+    //         })     
+    //     }
+    // }
+
+
 
     render() {  
-        //let mapped = this.props.data.map(e => console.log(e))
+        console.log(this.props.data)
+        const dataRender = () => {
+            for(let i = 0; i < this.props.data.length ; i++) {
+                if(this.props.year === this.props.data[i].year && this.props.month === this.props.data[i].month){
+                    return this.props.data[i].titles
+                }
+                else return 'error'
+            }
+        }
+
         const daysMap = days.map((item, i) => <div className="days" key={i}>{item}</div>)
         return (
+            //<Route path="/movies" 
             <div className="calendar">
             {daysMap} 
-            {this.state.calendarArr}
+            {this.props.data !== undefined && this.props.data.length > 0 ? dataRender() : ''} 
             </div>
         )
     }
