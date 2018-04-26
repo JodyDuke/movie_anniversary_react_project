b7a18e685d2297f84a720158faa5b6d09f1bbb67
Jody Duke <contact@jodyduke.co.uk>
2018-04-26 15:35:43 +0100

    added sort, links and extra styling to each node

--- src/components/main/calendar_node.js
+++ src/components/main/calendar_node.js
@@ -1,30 +1,49 @@
 import React,{ Component } from "react";
 import { numToMonth } from '../utils/num_to_month';
 import { tmdb } from '../../api/tmdb';
+import { Link } from 'react-router-dom';
+
 
 const TitleMap = (props) => {
     const titles = props.data
+    titles.sort((a, b) => b.vote_average - a.vote_average)
     const listTitles = titles.map((e, k) => <p key={k} className="titles">{e.birthday} - {e.title}</p>)
-    return <div>{listTitles}</div>
+    let moviePlural = listTitles.length > 2 ? 'movies' :  'movie'
+    return (
+        <div>
+            {listTitles[0]}
+            {listTitles.length > 1 ? <p style={{ color: 'white', fontSize: '70%' }}>+ {listTitles.length - 1} more {moviePlural}</p> : ''}
+        </div>
+    )
 }
 
 export default class CalendarNode extends Component {
+
     render() {
          let poster;
-        if(this.props.titles !== undefined && this.props.titles.length > 0){
+        if(this.props.titles !== undefined && this.props.titles.length > 0 && window.innerWidth > 768){
+            this.props.titles.sort((a, b) => b.vote_average - a.vote_average)
             poster = tmdb.images.secure_base_url + tmdb.images.poster_sizes[5] + this.props.titles[0].poster_path
         }
-
+        else if (this.props.titles !== undefined && this.props.titles.length > 0 && window.innerWidth <= 768) {
+            poster = tmdb.images.secure_base_url + tmdb.images.backdrop_sizes[1] + this.props.titles[0].backdrop_path
+        }
 
         return (
+            
             <div className="background-images" style={{backgroundImage: `url(${poster})`}}>
+                <Link to={{
+                    pathname : '/' + this.props.year + '/' + this.props.month + '/' + this.props.day,
+                    modal : true
+                    }}>
+
                 <div className={this.props.class}>
                     <h4>{this.props.day}</h4>
                     {this.props.titles ? <TitleMap data={this.props.titles} /> : null}
-                    <h3>{numToMonth(this.props.month)}</h3>
-                    <h3>{this.props.year}</h3>
                 </div>
+            </Link>
             </div>
+
         )
     }
 }
