ffd1be125dac738af4cac160ffe51d47d317d6c8
Jody Duke <contact@jodyduke.co.uk>
2018-04-28 16:39:48 +0100

    added other titles component to choose between titles in a calendar node

--- src/components/main/modal.js
+++ src/components/main/modal.js
@@ -8,16 +8,14 @@ import XSVG from '../../images/x';
 class Modal extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            nodeTitles : []
-        }
 
         this.closeModal = this.closeModal.bind(this)
+        this.changeTitle = this.changeTitle.bind(this)
     }
 
     componentWillMount(){
         const data = this.props.data
-        const urlString = window.location.pathname.replace('/', '')
+        const urlString = window.location.pathname.replace('/movies/', '')
         for(let i = 0 ; i < data.length ; i++){
             let currentMonth = data[i]
             for(let e = 0 ; e < currentMonth.titles.length ; e++){
@@ -32,7 +30,6 @@ class Modal extends Component {
     }
 
     componentDidMount() {
-        console.log(tmdb.url + tmdb.movie + this.state.currentTitle.id + tmdb.videos + config.TMDB_KEY)
         fetch(tmdb.url + tmdb.movie + this.state.currentTitle.id + tmdb.videos + config.TMDB_KEY, {
             callback: 'test'
         })
@@ -40,6 +37,13 @@ class Modal extends Component {
         .then(data => this.setState({
             videoId : data.results[0].key 
         }))
+    }
+
+    changeTitle(e) {
+        let newCurrentTitle = this.state.nodeTitles.filter(obj => obj.id === parseInt(e.currentTarget.id, 10))
+        this.setState({
+            currentTitle : newCurrentTitle[0]
+        })
     }
 
     closeModal(e){
@@ -50,7 +54,6 @@ class Modal extends Component {
     render(){
         let posterImage = tmdb.images.secure_base_url + tmdb.images.backdrop_sizes[2] + this.state.currentTitle.backdrop_path
 
-
         return(
             <div className="modal-body" style={{ backgroundImage: `url(${posterImage})` }}>
                 <div className="modal">
@@ -58,17 +61,34 @@ class Modal extends Component {
                         <div className="close-bar">
                             <div className="close" onClick={this.closeModal}><XSVG /></div>
                         </div>
-                        <div className="video">
+                        {this.state.nodeTitles.length > 1 ? <OtherTitles data={this.state.nodeTitles} titleChange={this.changeTitle} /> : null}
+
+                        {/* <div className="video">
                             <iframe title="trailer"
                                 src={'https://www.youtube.com/embed/' + this.state.videoId}>
                             </iframe>
-                        </div>
+                        </div> */}
 
                     </div>
                 </div>
             </div>
         )
+
     }
 }
 
 export default withRouter(Modal)
+
+class OtherTitles extends Component {
+    render(){
+        return (
+            <div className="multi-title-bar">
+                {this.props.data.map((e, k) => {
+                    return <div key={e.id} id={e.id} onClick={(e) => this.props.titleChange(e)}>
+                        <h5 style={{color: 'white'}}>{e.title}</h5>
+                    </div>
+                })}
+            </div>
+        )
+    }
+}
