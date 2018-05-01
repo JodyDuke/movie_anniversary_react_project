a71098668a6bb928f3c5e122b68e6a0c13e093c7
Jody Duke <contact@jodyduke.co.uk>
2018-05-01 17:27:51 +0100

    added cast, revenue, blurb to modals

--- src/components/main/modal.js
+++ src/components/main/modal.js
@@ -3,7 +3,10 @@ import { withRouter } from 'react-router-dom';
 import { tmdb } from '../../api/tmdb';
 import { config } from '../../api/api_keys';
 import XSVG from '../../images/x';
-import { numToMonth } from '../utils/num_to_month'
+import { numToMonth } from '../utils/num_to_month';
+import LinkSVG from '../../images/link';
+import { numberWithCommas } from '../utils/num_with_commas';
+import profile from '../../images/profile.png';
 
 
 class Modal extends Component {
@@ -26,27 +29,64 @@ class Modal extends Component {
                         date : new Date(urlString),
                         nodeTitles : currentMonth.titles[e].props.titles,
                         currentTitle: currentMonth.titles[e].props.titles[0]
-                    }, () => console.log(this.state.nodeTitles))
+                    })
                 }
             }
         }
     }
 
     componentDidMount() {
-        fetch(tmdb.url + tmdb.movie + this.state.currentTitle.id + tmdb.videos + config.TMDB_KEY, {
-            callback: 'test'
-        })
-        .then(response => response.json())
-        .then(data => this.setState({
-            videoId : data.results[0].key 
-        }))
+        const url = [
+            tmdb.url + tmdb.movie + this.state.currentTitle.id + tmdb.videos + config.TMDB_KEY,
+            tmdb.url + tmdb.movie + this.state.currentTitle.id + tmdb.credits + config.TMDB_KEY,
+            tmdb.url + tmdb.movie + this.state.currentTitle.id + '?' + config.TMDB_KEY
+        ];
+        Promise.all(
+            url.map(e => {
+                return fetch(e, {
+                    callback: 'test'
+                })
+                .then(response => response.json())
+            })
+        )
+        .then(data => {
+            const video = data[0].results[0].key
+            const credits = data[1]
+ 
+            this.setState({
+             currentTitleCredits: credits,
+             currentTitleVideoId: video,
+             currentTitleMiscInfo: data[2]
+            })}
+        )
     }
 
     changeTitle(e) {
-        let newCurrentTitle = this.state.nodeTitles.filter(obj => obj.id === parseInt(e.currentTarget.id, 10))
-        this.setState({
-            currentTitle : newCurrentTitle[0]
-        })
+        const newCurrentTitle = this.state.nodeTitles.filter(obj => obj.id === parseInt(e.currentTarget.id, 10))
+
+        const url = [
+            tmdb.url + tmdb.movie + newCurrentTitle[0].id + tmdb.videos + config.TMDB_KEY,
+            tmdb.url + tmdb.movie + newCurrentTitle[0].id + tmdb.credits + config.TMDB_KEY,
+            tmdb.url + tmdb.movie + newCurrentTitle[0].id + '?' + config.TMDB_KEY
+            
+        ];
+
+        Promise.all(
+            url.map(e => {
+                return fetch(e, {
+                    callback: 'test'
+                })
+                    .then(response => response.json())
+            })
+        )
+            .then(data => {
+                this.setState({
+                    currentTitleCredits: data[1],
+                    currentTitleMiscInfo: data[2],
+                    currentTitleVideoId: data[0].results[0].key,
+                    currentTitle: newCurrentTitle[0]
+                })
+            })
     }
 
     closeModal(e){
@@ -60,6 +100,7 @@ class Modal extends Component {
 
     render(){
         let posterImage = tmdb.images.secure_base_url + tmdb.images.backdrop_sizes[2] + this.state.currentTitle.backdrop_path
+        let currentTitleImage = tmdb.images.secure_base_url + tmdb.images.poster_sizes[1] + this.state.currentTitle.poster_path
         return(
             <div className="modal-body" style={{ backgroundImage: `url(${posterImage})` }}>
                 <div className="modal">
@@ -69,12 +110,46 @@ class Modal extends Component {
                             <div className="close" onClick={this.closeModal}><XSVG /></div>
                         </div>
 
-                        {this.state.nodeTitles.length > 1 ? <OtherTitles data={this.state} titleChange={this.changeTitle} /> : null}                                            
+                        {this.state.nodeTitles.length > 1 ? <OtherTitles data={this.state} titleChange={this.changeTitle} /> : null}  
 
                         <div className="current-title-info">
-                        <h1>{this.state.currentTitle.title}</h1>
-                        <h3>{this.state.currentTitle.birthday}<sup>th</sup> Anniversary</h3>
-                        <p>{this.state.currentTitle.overview}</p>
+                            <h1>{this.state.currentTitle.title}</h1>
+                            <h3>{this.state.currentTitle.birthday}<sup>th</sup> Anniversary</h3>
+                            <p>{this.state.currentTitle.overview}</p>
+                            {this.state.currentTitleCredits !== undefined ?
+                                <div className="cast">
+                                    <h3>Cast</h3>
+                                    {this.state.currentTitleCredits.cast.map((e, k) => {
+                                        let currentProfileImage = profile
+                                        if(e.profile_path !== null){
+                                        currentProfileImage = tmdb.images.secure_base_url + tmdb.images.poster_sizes[1] + e.profile_path  
+                                        }                              
+                                        return <div key={k} className="cast-node">
+                                            <img src={currentProfileImage} alt={e.name} />
+                                            <p>{e.name}</p>
+                                        </div>
+                                    })}
+                                </div>
+                                :
+                                null
+                            }
+                        </div>
+
+                        <div className="current-title-image">
+                            <img alt={this.state.currentTitle.title + " poster"} src={currentTitleImage} />
+                            <div className="trailer-link">
+                                <a target="_blank" href={'https://www.youtube.com/watch?v=' + this.state.currentTitleVideoId}>Trailer </a> 
+                                <LinkSVG />
+                            </div>
+                            {this.state.currentTitleMiscInfo !== undefined ? 
+                                <div className="budget">
+                                    <p>Budget<br />$ {numberWithCommas(this.state.currentTitleMiscInfo.budget)}</p>
+                                    <p>Revenue<br/>$ {numberWithCommas(this.state.currentTitleMiscInfo.revenue)}</p>
+                                </div>
+                                :
+                                ''
+                            }
+
                         </div>
                         {/* <div className="video">
                             <iframe title="trailer"
