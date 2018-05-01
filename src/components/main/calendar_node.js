b3626c35c01e5e8ae18ef4bbc9a33cdb517eb34b
Jody Duke <contact@jodyduke.co.uk>
2018-05-01 20:34:17 +0100

    title formatting

--- src/components/main/calendar_node.js
+++ src/components/main/calendar_node.js
@@ -9,12 +9,17 @@ const TitleMap = (props) => {
     const titles = props.data
     //sorting function that uses the IMDB algorithm for finding the mean vote for each title and ranking accordingly
         titles.sort((a, b) => weightedRanking(b, findMean(titles)) - weightedRanking(a, findMean(titles)))
-    const listTitles = titles.map((e, k) => <p key={k} className="titles">{e.birthday} - {e.title}</p>)
+    const listTitles = titles.map((e, k) => {
+        return <div className="title-info" key={k}>
+            <p className="birthday">{e.birthday}</p>
+            <p className="title">{e.title}</p>
+        </div>
+    })
     let moviePlural = listTitles.length > 2 ? 'movies' :  'movie'
     return (
         <div>
             {listTitles[0]}
-            {listTitles.length > 1 ? <p style={{ color: 'white', fontSize: '70%' }}>+ {listTitles.length - 1} more {moviePlural}</p> : ''}
+            {listTitles.length > 1 ? <p className="more-movies">+ {listTitles.length - 1} more {moviePlural}</p> : ''}
         </div>
     )
 }
