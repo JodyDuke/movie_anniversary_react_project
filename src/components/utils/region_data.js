19d76847008f34e86bedccf418944e7e58dea476
Jody Duke <contact@jodyduke.co.uk>
2018-05-01 17:28:15 +0100

    added more countries

--- src/components/utils/region_data.js
+++ src/components/utils/region_data.js
@@ -1,11 +1,7 @@
 export const regionSelectData = [
     {
-    "iso_3166_1": "GB",
-    "english_name": "United Kingdom"
-    },
-    {
-        "english_name": 'United States',
-        "iso_3166_1": 'US'
+        "iso_3166_1": "AU",
+        "english_name": "Australia"
     },
     {
         "iso_3166_1": "FR",
@@ -16,7 +12,31 @@ export const regionSelectData = [
         "english_name": "Germany"
     },
     {
-        "iso_3166_1": "AU",
-        "english_name": "Australia"
+        "iso_3166_1": "IE",
+        "english_name": "Ireland"
+    },
+    {
+        "iso_3166_1": "JP",
+        "english_name": "Japan"
+    },
+    {
+        "iso_3166_1": "NL",
+        "english_name": "Netherlands"
+    },
+    {
+        "iso_3166_1": "NO",
+        "english_name": "Norway"
+    },
+    {
+        "iso_3166_1": "ES",
+        "english_name": "Spain"
+    },
+    {
+        "iso_3166_1": "GB",
+        "english_name": "United Kingdom"
+    },
+    {
+        "english_name": 'United States of America',
+        "iso_3166_1": 'US'
     },
 ]
