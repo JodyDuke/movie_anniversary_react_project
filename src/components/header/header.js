6ec744ad0df606a027eadd7ff505eafd1829b0f0
Jody Duke <contact@jodyduke.co.uk>
2018-05-03 16:53:05 +0100

    added logic to handle changes based on url

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,50 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import { numToMonth } from '../utils/num_to_month';
 import ChevronLeft from '../../images/chevron_left';
 import ChevronRight from '../../images/chevron_right';
+import SearchBar from './search_bar';
+import XSVG from '../../images/x';
 
 class Header extends Component {
     constructor(props){
         super(props);
         
         this.handleClick = this.handleClick.bind(this)
+        this.search = this.search.bind(this)
     }
 
     handleClick = (data) => {
-        this.props.handleClickFromChild(data)
+        this.props.handleDateChange(data)
+    }
+
+    search(props){
+        this.props.searchQuery(props)
     }
 
     render() {
         return (
             <div className="header">
-                <div className="month">
-                    <button onClick={() => this.handleClick('monthDown')}><ChevronLeft/></button>{numToMonth(this.props.month)}<button onClick={() => this.handleClick('monthUp')}><ChevronRight/></button>
-                </div>
+                <SearchBar search={this.search} />
 
-                <div className="year">
-                    <button onClick={() => this.handleClick('yearDown')}><ChevronLeft /></button>{this.props.year}<button onClick={() => this.handleClick('yearUp')}><ChevronRight /></button>
-                </div>
+                {window.location.pathname === '/search' ? 
+                    <div className="search-close">
+                        <Link to="/">
+                            <XSVG />
+                        </Link>
+                    </div>
+                :     
+                    <div className="date-input">
+                        <div className="month">
+                            <button onClick={() => this.handleClick('monthDown')}><ChevronLeft/></button>{numToMonth(this.props.month)}<button onClick={() => this.handleClick('monthUp')}><ChevronRight/></button>
+                        </div>
+
+                        <div className="year">
+                            <button onClick={() => this.handleClick('yearDown')}><ChevronLeft /></button>{this.props.year}<button onClick={() => this.handleClick('yearUp')}><ChevronRight /></button>
+                        </div>
+                    </div>
+                }
+
             </div>
         )
     }
